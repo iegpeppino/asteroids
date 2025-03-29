059379de6e9549463e5b3ce0e91119b466e9499a
iegpeppino <iegpeppino@outlook.com.ar>
2025-03-29 20:04:09 +0000

    C: Update player.py main.py to draw static player sprite

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    # Start Game
     pygame.init()
+
+    # Initiate clock with arg for 60 fps freq
     clock = pygame.time.Clock()
     dt = 0
     tick_delta = clock.tick(60)
-    dt = delta / 1000
+    dt = tick_delta / 1000
     
+    # Initialize Screen Object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create Instance of Player object
+    player = Player(x= SCREEN_WIDTH /2, y= SCREEN_HEIGHT /2)
+    
+
+    # Main Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        pygame.Surface.fill(screen, (0,0,0)) # Fill Screen with black
+        player.draw(screen) # Draw Player
         pygame.display.flip()
+
+    # Testing Console Outputs
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
